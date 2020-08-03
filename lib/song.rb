740da2436db945ac8f21519c3bfaea63d6d55867
Courtney Yargo <courtneyyargo@gmail.com>
2020-08-03 17:32:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+    
   def self.all
     @@all
   end
@@ -10,4 +10,23 @@ class Song
     self.class.all << self
   end
 
+  def self.create 
+    song = self.new 
+    @@all << song
+    song 
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    @@all << song 
+    song
+  end 
+    
 end
